9c669f9ffb4b60e97dd85d07a599b02f29432c0b
MarcOliva <ing.oliva@hotmail.com>
2022-09-18 07:17:23 -0500

    Fixed android implementation and change to deprecated props

--- js/Menu.js
+++ js/Menu.js
@@ -1,21 +1,21 @@
-import React, { Component } from "react"
-import { ViewPropTypes } from "react-native"
+import React, { Component } from 'react'
+import { ViewPropTypes } from 'deprecated-react-native-prop-types'
 
-import PropTypes from "prop-types"
+import PropTypes from 'prop-types'
 
-
-class Menu extends Component {
-
-}
+class Menu extends Component {}
 
 Menu.propTypes = {
   ...ViewPropTypes,
 
   label: PropTypes.string,
-  icon: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.object])
-};
+  icon: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+    PropTypes.object,
+  ]),
+}
 
-Menu.defaultProps = {
-};
+Menu.defaultProps = {}
 
-export { Menu  };
+export { Menu }
--- js/RNPopoverMenu.js
+++ js/RNPopoverMenu.js
@@ -1,12 +1,13 @@
-import React, { PureComponent } from "react";
-import { findNodeHandle, ViewPropTypes, NativeModules } from "react-native";
-import PropTypes from "prop-types";
+import React, { PureComponent } from 'react'
+import { findNodeHandle, NativeModules } from 'react-native'
+import PropTypes from 'prop-types'
+import { ViewPropTypes } from 'deprecated-react-native-prop-types'
 
-import RNImageHelper from "react-native-image-helper"
+import RNImageHelper from 'react-native-image-helper'
 
-import { Menu } from "./Menu";
+import { Menu } from './Menu'
 
-const { RNPopoverMenu } = NativeModules;
+const { RNPopoverMenu } = NativeModules
 
 class Popover extends PureComponent {
   static propTypes = {
@@ -32,16 +33,16 @@ class Popover extends PureComponent {
     shadowOpacity: PropTypes.number,
     shadowRadius: PropTypes.number,
     shadowOffsetX: PropTypes.number,
-    shadowOffsetY: PropTypes.number
-  };
+    shadowOffsetY: PropTypes.number,
+  }
 
   static defaultProps = {
     visible: false,
-    title: "",
-    tintColor: "#FFFFFF",
-    textColor: "",
-    borderColor: "",
-    separatorColor: "",
+    title: '',
+    tintColor: '#FFFFFF',
+    textColor: '',
+    borderColor: '',
+    separatorColor: '',
     borderWidth: 1,
     selectedRowBackgroundColor: '',
     roundedArrow: true,
@@ -50,98 +51,99 @@ class Popover extends PureComponent {
     menuWidth: 120,
     rowHeight: 40,
     menus: [],
-    theme: "light",
+    theme: 'light',
     shadowColor: '#000000',
     shadowOpacity: 0,
     shadowRadius: 5,
     shadowOffsetX: 0,
-    shadowOffsetY: 2
-  };
+    shadowOffsetY: 2,
+  }
 
   static Show(ref, props) {
-
     // unified default props handler
     Object.keys(Popover.defaultProps).map((id) => {
-      if(props[id] === undefined) props[id] = Popover.defaultProps[id];
-    });
-
+      if (props[id] === undefined) props[id] = Popover.defaultProps[id]
+    })
 
     props.menus &&
-      props.menus.forEach(menu => {
+      props.menus.forEach((menu) => {
         menu.menus &&
-          menu.menus.forEach(subMenu => {
+          menu.menus.forEach((subMenu) => {
             if (subMenu.icon && subMenu.icon.props) {
-              subMenu.icon = subMenu.icon.props;
+              subMenu.icon = subMenu.icon.props
 
               let vectorIcon = RNImageHelper.Resolve(
                 subMenu.icon.family,
                 subMenu.icon.name
-              );
-              subMenu.icon = Object.assign({}, subMenu.icon, vectorIcon);
+              )
+              subMenu.icon = Object.assign({}, subMenu.icon, vectorIcon)
             } else if (subMenu.icon !== undefined) {
               subMenu.icon = {
                 name: subMenu.icon,
-                family: "",
-                glyph: "",
-                color: "",
-                size: 0
-              };
+                family: '',
+                glyph: '',
+                color: '',
+                size: 0,
+              }
             }
-          });
+          })
 
         if (menu.icon && menu.icon.props) {
-          menu.icon = menu.icon.props;
+          menu.icon = menu.icon.props
 
-          let vectorIcon = RNImageHelper.Resolve(menu.icon.family, menu.icon.name);
-          menu.icon = Object.assign({}, menu.icon, vectorIcon);
+          let vectorIcon = RNImageHelper.Resolve(
+            menu.icon.family,
+            menu.icon.name
+          )
+          menu.icon = Object.assign({}, menu.icon, vectorIcon)
         } else if (menu.icon !== undefined) {
           menu.icon = {
             name: menu.icon,
-            family: "",
-            glyph: "",
-            color: "",
-            size: 0
-          };
+            family: '',
+            glyph: '',
+            color: '',
+            size: 0,
+          }
         }
-      });
+      })
 
     RNPopoverMenu.Show(
       findNodeHandle(ref),
       { ...props },
       (index, menuIndex) => {
-        props.onDone && props.onDone(index, menuIndex);
+        props.onDone && props.onDone(index, menuIndex)
       }
-    );
+    )
   }
 
   componentDidMount() {
-    this._show();
+    this._show()
   }
 
   componentDidUpdate() {
-    this._show();
+    this._show()
   }
 
   _show() {
-    let menus = [];
+    let menus = []
 
     React.Children.map(
       this.props.children,
       (mainItem, mainIndex, mainItems) => {
-        let subMenus = [];
+        let subMenus = []
         React.Children.map(mainItem.props.children, (item, index, items) => {
           subMenus.push({
             label: item.props.label,
-            icon: item.props.icon
-          });
-        });
+            icon: item.props.icon,
+          })
+        })
 
         menus.push({
           label: mainItem.props.label,
-          menus: subMenus
-        });
+          menus: subMenus,
+        })
       }
-    );
+    )
 
     if (this.props.visible) {
       Popover.Show(this.props.reference, {
@@ -163,17 +165,16 @@ class Popover extends PureComponent {
         shadowRadius: this.props.shadowRadius,
         shadowOffsetX: this.props.shadowOffsetX,
         shadowOffsetY: this.props.shadowOffsetY,
-        onDone: this.props.onDone
-      });
+        onDone: this.props.onDone,
+      })
     }
   }
 
   render() {
-    return null;
+    return null
   }
 }
 
+Popover.Menu = Menu
 
-Popover.Menu = Menu;
-
-export default Popover;
+export default Popover
